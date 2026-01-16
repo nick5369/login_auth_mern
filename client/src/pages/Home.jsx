281602161d3e38852e762f7307663f9b11a2d45b
Nick Patel <nickpatel17042006@gmail.com>
2026-01-16 13:05:35 +0530

    env bug fixed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import Hero from "../components/Hero";
-import Instructor from "../components/Instructor";
-import Technologies from "../components/Technologies";
 import "../styles/Home.css";
 import { toast } from "react-toastify";
 import axios from "axios";
 import { Navigate, useNavigate } from "react-router-dom";
-import Footer from "../layout/Footer";
 import { useAuthContext } from "../context/authContext.jsx";
 
 const Home = () => {
